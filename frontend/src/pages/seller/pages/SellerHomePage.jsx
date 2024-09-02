a4efe9017729da188cef2597e27f61b5f7d3fd68
Siddharth Vijay Gadge <142971662+v-siddarth@users.noreply.github.com>
2024-09-02 15:26:06 +0530

    vercel update

--- frontend/src/pages/seller/pages/SellerHomePage.jsx
+++ frontend/src/pages/seller/pages/SellerHomePage.jsx
@@ -1,26 +1,37 @@
 import { Grid } from '@mui/material';
 import SalesCard from '../components/SalesCard';
 import SalesChart from '../components/SalesChart';
+import { useSelector } from 'react-redux';
 
 const SellerHomePage = () => {
+  // Retrieve data from the Redux store
+  const { specificProductData } = useSelector(state => state.user);
+
+  // Calculate totals
+  const addedToCartTotal = specificProductData ? specificProductData.length : 0;
+  const ongoingOrdersTotal = specificProductData
+    ? specificProductData.filter(product => product.status === 'ongoing').length
+    : 0;
+  const outForDeliveryTotal = specificProductData
+    ? specificProductData.filter(product => product.status === 'outForDelivery').length
+    : 0;
 
   return (
     <Grid container spacing={3} sx={{ padding: "9px" }}>
-
       <Grid item xs={12} sm={6} md={3}>
         <SalesCard title="Weekly Sales" total={0} color='primary' icon={'ant-design:carry-out-filled'} />
       </Grid>
 
       <Grid item xs={12} sm={6} md={3}>
-        <SalesCard title="Added to Cart" total={0} color="success" icon={'ant-design:shopping-cart-outlined'} />
+        <SalesCard title="Added to Cart" total={addedToCartTotal} color="success" icon={'ant-design:shopping-cart-outlined'} />
       </Grid>
 
       <Grid item xs={12} sm={6} md={3}>
-        <SalesCard title="Ongoing Orders" total={0} color="warning" icon={'material-symbols:data-exploration'} />
+        <SalesCard title="Ongoing Orders" total={ongoingOrdersTotal} color="warning" icon={'material-symbols:data-exploration'} />
       </Grid>
 
       <Grid item xs={12} sm={6} md={3}>
-        <SalesCard title="Cancelled Orders" total={0} color="error" icon={'material-symbols:free-cancellation-rounded'} />
+        <SalesCard title="Out for Delivery" total={outForDeliveryTotal} color="info" icon={'material-symbols:local-shipping'} />
       </Grid>
 
       <Grid item xs={12} lg={6}>
